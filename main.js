2bfa38e94cfa27c67a6c9b9fc0ea36adaffbd4f8
Thanuk <86400849+Thanuk@users.noreply.github.com>
2022-03-11 20:22:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.position(950, 150);
+    video = createCapture(VIDEO);
+    video.size(550, 600);
+}
+
+function draw(){
+    
+}
